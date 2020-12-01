07321477c53454ccb2b7980422a4fa6019122cfc
hsmak <husain.alkhamis@gmail.com>
2020-11-30 22:49:47 -0600

    committing changes..

--- spring-framework-5/spring-mvc/src/main/java/org/hsmak/controller/PlainTextController.java
+++ spring-framework-5/spring-mvc/src/main/java/org/hsmak/controller/PlainTextController.java
@@ -2,7 +2,9 @@ package org.hsmak.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,7 +14,15 @@ public class PlainTextController {
             value = "/text",
             produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody // Make sure Jackson jars are on the classpath
-    public String getText() {
-        return "This is a String";
+    public String getText(@RequestParam(required = false) String id) {
+        return "This is a String by id: " + id;
+    }
+
+    @RequestMapping(
+            value = "/text/{name}",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody // Make sure Jackson jars are on the classpath
+    public String getTextByPath(@PathVariable String name, @RequestParam(required = false) String id) {
+        return String.format("This is a String by name (%s) and id(%s)", name, id);
     }
 }
